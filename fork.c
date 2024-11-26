894107d46dd9c021e1288b9c7b4836ead04458ce
Joy Herman <jherman50@stuy.edu>
2024-11-26 11:44:57 -0500

    doesnt work

--- fork.c
+++ fork.c
@@ -16,31 +16,37 @@ int err(){
     exit(1);
 }
 
-int randomInt() {
-    int randfile;
-    int randint;
-    randfile = open("/dev/random", O_RDONLY, 0);
-    if(randfile == -1) {
-        err();
-    }
-    return randint;
+int randomInt(randfile) {
+	int t;
+		int *randint;
+		read(randfile, randint, 4);
+	  t = abs(*randint) % 5;
+		return t;
 }
 
 int main() {
-  printf("im in main\n");
-  pid_t p, q;
-  printf("%d about to create 2 child processes\n", getpid());
-  p = fork();
-  if(p<0){
-    perror("fork fail");
-    exit(1);
-  } else if (p == 0){
-    printf("%d %dsec\n", getpid(), randomInt());
-    q = fork();
-    printf("%d %dsec\n", getpid(), randomInt());
-    printf("Hello from Child!\n");
-  }else{
-    printf("Hello from Parent!\n");
+	int randfile;
+	randfile = open("/dev/random", O_RDONLY, 0);
+	if(randfile == -1) {
+			err();
+	}
+	pid_t p, q;
+	printf("%d about to create 2 child processes\n", getpid());
+	p = fork();
+	if(p<0){
+		perror("fork fail");
+		exit(1);
+	} else if (p == 0){
+		int s = randomInt(randfile);
+		printf("%d %dsec\n", getpid(), s);
+		sleep(s);
+		printf("%d finished after %d seconds\n", getpid(), s);
+	} else{
+		q = fork();
+		int s = randomInt(randfile);
+		printf("%d %dsec\n", getpid(), s);
+		sleep(s);
+		printf("%d finished after %d seconds\n", getpid(), s);
   }
   return 0;
 }
